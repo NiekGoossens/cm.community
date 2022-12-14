e494ed8ce6e31618e10e9d9561a9048c8d1c505e
Niekelodius <niekelodius@gmail.com>
2022-12-14 15:13:19 +0100

    added header and schema

---
+++ apps/cm.community/src/app/shared/header/header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cm-community-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent {}
